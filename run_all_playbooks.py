d072cf7eba7ae079444535ae5aa42ec95185fd3d
BKW <brandon.k.whitmire@gmail.com>
2022-02-07 15:41:29 -0600

    minor beautification of output

--- run_all_playbooks.py
+++ run_all_playbooks.py
@@ -8,5 +8,6 @@ import ansible_runner
 
 # Find all files ending in `.yml` and execute them with Ansible
 for playbook in glob.glob("**/*.yml", recursive=True):
+    print("=" * 80)
     print(f"Running {playbook} ...")
     ansible_runner.run(private_data_dir=".", playbook=playbook)
