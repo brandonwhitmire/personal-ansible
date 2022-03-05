e804fb65c8b824369976b4fa3c3fcc319bdb5c04
BKW <brandon.k.whitmire@gmail.com>
2022-03-05 09:08:19 -0600

    fix steam fixes; minor tweaks

--- run_all_playbooks.py
+++ run_all_playbooks.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python3
 """
 Simple script to run all playbooks
+
+Reference:
+- https://ansible-runner.readthedocs.io/en/stable/
 """
 
 import glob
 import ansible_runner
 
-# Find all files ending in `.yml` and execute them with Ansible
-for playbook in glob.glob("**/*.yml", recursive=True):
-    print("=" * 80)
+PRIVATE_DATA_DIR = "."
+INVENTORY = "hosts"
+
+def neat_border(char="-", repeat=80):
+    """
+    Pretty borders for simple output
+    """
+    print(char * repeat)
+
+neat_border(char='=')
+
+# Show inventory
+print(ansible_runner.interface.get_inventory(action="list",
+                                             inventories=[INVENTORY]))
+
+# Find all files ending in '*.yml', ignoring some, and execute them with Ansible
+returns = []
+for playbook in [playbook for playbook in glob.glob("**/*.yml",
+                                                    recursive=True) if
+                 "function_" not in playbook]:
+    neat_border()
     print(f"Running {playbook} ...")
-    ansible_runner.run(private_data_dir=".", playbook=playbook)
+    returns.append(ansible_runner.run(private_data_dir=PRIVATE_DATA_DIR, playbook=playbook))
+
+# Show those delicious stats
+neat_border()
+print(returns)
+
+neat_border(char='=')
