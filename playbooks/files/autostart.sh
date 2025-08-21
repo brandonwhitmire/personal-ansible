1c015d2a7557fd28b07c7ad7d782cfbf57be6669
BKW <brandon.k.whitmire@gmail.com>
2025-08-20 18:28:13 -0600

    tweak i3 and move some logic to scripts

---
+++ playbooks/files/autostart.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# This script is executed by i3 on startup.
+
+# Terminate already running instances of programs
+killall -q picom
+killall -q dunst
+killall -q nitrogen
+killall -q nm-applet
+killall -q blueman-applet
+killall -q flameshot
+killall -q conky
+# Add other apps you want to ensure are killed before restarting
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+# Add other 'while' loops if you need to wait for specific apps
+
+# Set screen layout with xrandr (uncomment and customize if needed)
+# xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal &
+
+# Set wallpaper
+nitrogen --restore &
+
+# Launch compositor
+picom -b &
+
+# Launch notification daemon
+dunst &
+
+# Launch applets in the background
+nm-applet &
+blueman-applet &
+flameshot &
+
+# Launch password manager
+keepassxc &
+
+# Launch system monitor
+conky --config=/usr/share/conky/conky_theme &
+
+# Start PulseAudio and applet
+start-pulseaudio-x11 &
+pa-applet &
+
+# Set keyboard numlock state
+/usr/bin/numlockx on &
+
+# Start screen temperature utility
+/usr/bin/redshift-gtk &
+
+# Start screen locker daemon
+set $Locker "i3lock --ignore-empty-password --color=444444"
+xautolock -notify 30 -notifier "notify-send 'Screen will lock soon!'" -time 15 -locker "$Locker" &
+
+echo "Autostart script finished."
---
+++ playbooks/files/powermenu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# A dmenu-based power menu script for i3
+# Provides options for Lock, Suspend, Logout, Reboot, and Shutdown
+
+# Use rofi if available, otherwise dmenu
+# You can change this to `rofi -dmenu` or a different launcher
+LAUNCHER="dmenu -i -p Power"
+
+# Options to display in the menu
+# Using Nerd Font icons for a nicer look
+OPTIONS=" Lock\n Suspend\n Logout\n Reboot\n Shutdown"
+
+# Get the user's choice
+CHOSEN=$(echo -e "$OPTIONS" | $LAUNCHER)
+
+# Execute the corresponding command
+case "$CHOSEN" in
+    " Lock")
+        i3lock --ignore-empty-password --color=444444
+        ;;
+    " Suspend")
+        systemctl suspend
+        ;;
+    " Logout")
+        i3-msg exit
+        ;;
+    " Reboot")
+        systemctl reboot
+        ;;
+    " Shutdown")
+        systemctl poweroff -i
+        ;;
+esac
